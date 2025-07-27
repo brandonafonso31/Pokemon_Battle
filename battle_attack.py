22d5478c8b52a7cb373c776c1a27bccb0e3b4a4f
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-27 20:06:15 +0200

    multi corr: wrong log (print wrong move) and using wrong move everytime

--- battle_attack.py
+++ battle_attack.py
@@ -6,12 +6,12 @@ from random import randint
 def check_move(move_id: str):
     return move_id in ["move1","move2","move3","move4"]
 
-def check_prio(pokemon:Pokemon, move_id:str, pokemon_ia:Pokemon,move_id_ia):
+def check_prio(pokemon:Pokemon, move_id:str, pokemon_ia:Pokemon,move_id_ia:str):
     move = getattr(pokemon, move_id)
     move_ia = getattr(pokemon_ia,move_id_ia)
     if move_ia.prio > move.prio or pokemon_ia.vit > pokemon.vit:
-        return pokemon_ia,move_ia,pokemon,move
-    return pokemon,move,pokemon_ia,move_ia
+        return pokemon_ia,move_id_ia,pokemon,move_id
+    return pokemon,move_id,pokemon_ia,move_id_ia
 
 def perform_choice_attack(pokemon_1: Pokemon, pokemon_2: Pokemon, move_id: str,window,res_scene,resolution):
     pygame.draw.rect(window, BLACK, (0, res_scene[1], resolution[0], resolution[1]-res_scene[1]))
@@ -25,19 +25,21 @@ def perform_choice_attack(pokemon_1: Pokemon, pokemon_2: Pokemon, move_id: str,w
     if not check_move(move_id_ia):
         return Exception(f"Error: {move_id_ia} n'est pas un attribut du Pokemon Ennemi")
     
-    pokemon_1,move_1,pokemon_2,move_2 = check_prio(pokemon_1,move_id,pokemon_2,move_id_ia)
+    pokemon_1,move_id_1,pokemon_2,move_id_2 = check_prio(pokemon_1,move_id,pokemon_2,move_id_ia)
 
     # Attaque du Pokémon le plus rapide
     pokemon_1, pokemon_2 = pokemon_1.use_move(move_id,pokemon_2)
-    print(f"PP {pokemon_1.name} {move_id}: {pokemon_1.move1.pp}, Pv {pokemon_2.name}: {pokemon_2.pv}")  # barre de vie dans la fentre direct / pas d'affichage de pp
+    print(f"PP {pokemon_1.name} {move_id}: {pokemon_1.getattr(pokemon_1,move_id_1).pp}, Pv {pokemon_2.name}: {pokemon_2.pv}")  # barre de vie dans la fentre direct / pas d'affichage de pp
     pygame.time.delay(1500)
     
     # Si le Pokémon 2 (le moins rapide) n'est pas mort il attaque
     if not pokemon_2.is_dead():
-        pokemon_2, pokemon_1 = pokemon_2.use_move(move_id,pokemon_1)
-        print(f"PP {pokemon_2.name} {move_id}: {pokemon_2.move1.pp}, Pv {pokemon_1.name}: {pokemon_1.pv}")  # barre de vie dans la fentre direct / pas d'affichage de pp
+        pokemon_2, pokemon_1 = pokemon_2.use_move(move_id_2,pokemon_1)
+        print(f"PP {pokemon_2.name} {move_id_2}: {pokemon_2.getattr(pokemon_2,move_id_2).pp}, Pv {pokemon_1.name}: {pokemon_1.pv}")  # barre de vie dans la fentre direct / pas d'affichage de pp
             
     pygame.time.delay(500)
+    
+    
     return pokemon_1, pokemon_2, (not pokemon_2.is_dead() and not pokemon_1.is_dead())
 
 
