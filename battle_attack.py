5d70a4faff88970975dd65662b3ae959df977ea0
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-27 20:33:50 +0200

    add booleen for changing pokemon again to bypass changing movesets next turn

--- battle_attack.py
+++ battle_attack.py
@@ -12,10 +12,10 @@ def check_prio(pokemon:Pokemon, move_id:str, pokemon_ia:Pokemon,move_id_ia:str):
     move_ia = getattr(pokemon_ia,move_id_ia)
     print(move_ia,move_ia.prio)
     if move_ia.prio > move.prio or pokemon_ia.vit > pokemon.vit:
-        return pokemon_ia,move_id_ia,pokemon,move_id
-    return pokemon,move_id,pokemon_ia,move_id_ia
+        return pokemon_ia,move_id_ia,pokemon,move_id, True
+    return pokemon,move_id,pokemon_ia,move_id_ia,False
 
-def perform_choice_attack(pokemon_1: Pokemon, pokemon_2: Pokemon, move_id: str,window,res_scene,resolution):
+def perform_choice_attack(pokemon_1: Pokemon, pokemon_ia: Pokemon, move_id: str,window,res_scene,resolution):
     pygame.draw.rect(window, BLACK, (0, res_scene[1], resolution[0], resolution[1]-res_scene[1]))
     pygame.display.flip()
     
@@ -23,15 +23,15 @@ def perform_choice_attack(pokemon_1: Pokemon, pokemon_2: Pokemon, move_id: str,w
         return Exception(f"Error: {move_id} n'est pas un attribut du Pokemon Allié")
    
     # Changer ou non l'order selon la prio des attaques ou la vitesse des pokemon
-    move_id_ia = f"move{randint(1,len(pokemon_2.get_moveset()))}"   # a modifier plus tard si vrai reflextion
+    move_id_ia = f"move{randint(1,len(pokemon_ia.get_moveset()))}"   # a modifier plus tard si vrai reflextion
     if not check_move(move_id_ia):
         return Exception(f"Error: {move_id_ia} n'est pas un attribut du Pokemon Ennemi")
     
-    pokemon_1,move_id_1,pokemon_2,move_id_2 = check_prio(pokemon_1,move_id,pokemon_2,move_id_ia)
+    pokemon_1,move_id_1,pokemon_2,move_id_2,bool_change = check_prio(pokemon_1,move_id,pokemon_ia,move_id_ia)
 
     # Attaque du Pokémon le plus rapide
-    pokemon_1, pokemon_2 = pokemon_1.use_move(move_id,pokemon_2)
-    print(f"PP {pokemon_1.name} {move_id}: {getattr(pokemon_1,move_id_1).pp}, Pv {pokemon_2.name}: {pokemon_2.pv}\n")  # barre de vie dans la fentre direct / pas d'affichage de pp
+    pokemon_1, pokemon_2 = pokemon_1.use_move(move_id_1,pokemon_2)
+    print(f"PP {pokemon_1.name} {move_id_1}: {getattr(pokemon_1,move_id_1).pp}, Pv {pokemon_2.name}: {pokemon_2.pv}\n")  # barre de vie dans la fentre direct / pas d'affichage de pp
     pygame.time.delay(1500)
     
     # Si le Pokémon 2 (le moins rapide) n'est pas mort il attaque
@@ -41,7 +41,8 @@ def perform_choice_attack(pokemon_1: Pokemon, pokemon_2: Pokemon, move_id: str,w
             
     pygame.time.delay(500)
     
-    
+    if bool_change:
+        pokemon_1,pokemon_2 = pokemon_2, pokemon_1
     return pokemon_1, pokemon_2, (not pokemon_2.is_dead() and not pokemon_1.is_dead())
 
 
