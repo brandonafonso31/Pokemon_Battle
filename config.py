e2d79fdee5a63f7c5647d8a360ae391c6558102b
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-28 01:53:11 +0200

    add res

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
+res_scene = (1050,540)
+resolution = (res_scene[0],260+res_scene[1])
 principal_dir_path = os.getcwd()
 img_dir_path = os.path.join(principal_dir_path,"images")
 song_dir_path = os.path.join(principal_dir_path,"song")
