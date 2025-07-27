24e6142d6aa6966d86973f38480f3adb134d389c
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-27 14:59:59 +0200

    remove useless functioon check_if_dead bc already declared in Pokemon Class

--- battle_attack.py
+++ battle_attack.py
@@ -9,9 +9,6 @@ def attack(pokemon_attacker: Pokemon,pokemon_defender: Pokemon,move_id: str = "m
     pokemon_attacker,pokemon_defender = pokemon_attacker.use_move(move_id,pokemon_defender)
     return pokemon_attacker,pokemon_defender
 
-def check_if_dead(pokemon):
-    return pokemon.pv <= 0
-
 def turn(pokemon_1: Pokemon,pokemon_2: Pokemon, move_id: int):
     if move_id not in ["move1","move2","move3","move4"]:
         return
@@ -24,12 +21,12 @@ def turn(pokemon_1: Pokemon,pokemon_2: Pokemon, move_id: int):
     pokemon_1, pokemon_2 = attack(pokemon_1, pokemon_2, move_id, ia = (ia == pokemon_1))
     print(f"PP {pokemon_1.name} move1: {pokemon_1.move1.pp}, Pv {pokemon_2.name}: {pokemon_2.pv}")      # barre de vie dans la fentre direct / pas d'affichage de pp
     pygame.time.delay(2000)
-    if not check_if_dead(pokemon_2):
+    if not pokemon_2.is_dead():
         pokemon_2,pokemon_1= attack(pokemon_2, pokemon_1, move_id, ia = (ia == pokemon_2))
         print(f"PP {pokemon_2.name} move1: {pokemon_2.move1.pp}, Pv {pokemon_1.name}: {pokemon_1.pv}")  # barre de vie dans la fentre direct / pas d'affichage de pp
     else:
-        pokemon_1,pokemon_2,not check_if_dead(pokemon_2)   
-    return pokemon_1,pokemon_2,not check_if_dead(pokemon_2) and not check_if_dead(pokemon_1)
+        pokemon_1,pokemon_2,not pokemon_2.is_dead()   
+    return pokemon_1,pokemon_2,not pokemon_2.is_dead() and not pokemon_1.is_dead()
 
 def perform_choice_attack(pokemon_trainer, pokemon_opponent, move_id,window,res_scene,resolution):
     pygame.draw.rect(window, BLACK, (0, res_scene[1], resolution[0], resolution[1]-res_scene[1]))
@@ -46,7 +43,7 @@ def perform_choice_attack(pokemon_trainer, pokemon_opponent, move_id,window,res_
 """
 def battle(pokemon_trainer: Pokemon,pokemon_opponent: Pokemon):
     in_battle = True
-    while in_battle and not check_if_dead(pokemon_trainer) and not check_if_dead(pokemon_opponent):
+    while in_battle and not pokemon_trainer.is_dead() and not pokemon_opponent.is_dead():
         pokemon_trainer,pokemon_opponent, in_battle = turn(pokemon_trainer,pokemon_opponent)
     return pokemon_trainer,pokemon_opponent, in_battle
 """
