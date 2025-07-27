f1123d6f5e77804393aeb415b7b7d810711451f1
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-27 13:07:23 +0200

    add  text fonction

--- button.py
+++ button.py
@@ -1,14 +1,21 @@
 import pygame
 
 class Button():
-	def __init__(self, x, y, image, scale):
+	def __init__(self, x, y, image, scale, text='', text_color=(0, 0, 0)):
 		width = image.get_width()
 		height = image.get_height()
 		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
 		self.rect = self.image.get_rect()
 		self.rect.topleft = (x, y)
 		self.clicked = False
-
+  
+  		# Texte du bouton
+		self.text = text
+		self.font = pygame.font.SysFont(None, 24)  # Taille par défaut
+		self.text_color = text_color
+		self.text_surface = self.font.render(text, True, text_color)
+		self.text_rect = self.text_surface.get_rect(center=self.rect.center)
+  
 	def draw(self, surface):
 		action = False
 		#get mouse position
@@ -25,5 +32,8 @@ class Button():
 
 		#draw button on screen
 		surface.blit(self.image, (self.rect.x, self.rect.y))
-
+  
+		# Redessine le texte (car centré sur le rect original)
+		self.text_rect = self.text_surface.get_rect(center=self.rect.center)
+		surface.blit(self.text_surface, self.text_rect)
 		return action
