d42d658b7e42bcac07e0a348166181cf7fbc061b
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-28 00:10:32 +0200

    add test animation for lanceflamme

--- pokemon_init.py
+++ pokemon_init.py
@@ -2,12 +2,25 @@ from pokemon import Pokemon
 from pokemon_type import Type
 from pokemon_move import *
 from pokemon_nature import Nature
+import pygame
+
+#--------------------------------------| ANIMATION |--------------------------------------#
+def flash_red_animation(window, attacker_sprite, defender_sprite, attacker_coords, defender_coords):
+    for _ in range(2):
+        pygame.time.delay(100)
+        temp = defender_sprite.copy()
+        temp.fill((255, 0, 0), special_flags=pygame.BLEND_RGB_ADD)
+        window.blit(temp, defender_coords)
+        pygame.display.update()
+        pygame.time.delay(100)
+        window.blit(defender_sprite, defender_coords)
+        pygame.display.update()        
 #--------------------------------------| Dracaufeu |--------------------------------------#
 dracaufeu_EV = {"pv":0,"atk":0,"def_":0,"atk_spe":252,"def_spe":6,"vit":252}
 dracaufeu = Pokemon("Dracaufeu",pv=78,atk=84,def_=78,atk_spe=109,def_spe=85,vit=100,gen=1,
                     type1=Type.FEU,type2=Type.VOL,talent="brasier",num_on_sprite_sheet=7,EV=dracaufeu_EV,nature=Nature.MODESTE)
 
-lance_flamme = SpecialMove("Lance-Flammes",Type.FEU,power=90,precision=100,pp=15, effect = dico_effect_move["Lance-Flammes"], prio = 0)
+lance_flamme = SpecialMove("Lance-Flammes",Type.FEU,power=90,precision=100,pp=15, effect = dico_effect_move["Lance-Flammes"], prio = 0,animation=flash_red_animation)
 crocs_eclair = SpecialMove("Croc-éclair",Type.ELECTRIQUE,power=90,precision=100,pp=15, effect = dico_effect_move["Croc-éclair"], prio = 0)
 lance_soleil = SpecialMove("Lance-Soleil",Type.PLANTE,power=90,precision=100,pp=15, effect = dico_effect_move["Lance-Soleil"], prio = 0)
 atterisage = StatusMove("Atterisage",Type.VOL,precision=100,pp=15, effect = dico_effect_move["Atterissage"], prio = 0)
