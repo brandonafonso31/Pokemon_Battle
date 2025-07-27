bf77c214ff56eb29b72d73d23fcfc3126d0ee837
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-27 19:56:46 +0200

    add bool in each if to bypass previous changes on pokemon.get_moveset()

--- ui_battle.py
+++ ui_battle.py
@@ -17,13 +17,14 @@ def choice_move(window,res_scene,resolution,x_move,y_menu,pokemon_trainer,pokemo
     still_in_battle = True
                 
     moves = pokemon_trainer.get_moveset()
-    if moves[0] is not None and draw_move(window,moves[0],x_move - 200, y_menu + 50):
+    length = len(moves)
+    if length > 0 and moves[0] is not None and draw_move(window,moves[0],x_move - 200, y_menu + 50):
         pokemon_trainer, pokemon_opponent, still_in_battle = battle_attack.perform_choice_attack(pokemon_trainer, pokemon_opponent, "move1",window,res_scene,resolution)                            
-    if moves[1] is not None and draw_move(window,moves[1],x_move + 200, y_menu + 50):
+    if length > 1 and moves[1] is not None and draw_move(window,moves[1],x_move + 200, y_menu + 50):
         pokemon_trainer, pokemon_opponent, still_in_battle = battle_attack.perform_choice_attack(pokemon_trainer, pokemon_opponent, "move2",window,res_scene,resolution)                
-    if moves[2] is not None and draw_move(window,moves[2],x_move - 200, y_menu + 150):
+    if length > 2 and moves[2] is not None and draw_move(window,moves[2],x_move - 200, y_menu + 150):
         pokemon_trainer, pokemon_opponent, still_in_battle = battle_attack.perform_choice_attack(pokemon_trainer, pokemon_opponent, "move3", window,res_scene,resolution)                
-    if moves[3] is not None and draw_move(window,moves[3],x_move + 200, y_menu + 150):
+    if length > 3 and moves[3] is not None and draw_move(window,moves[3],x_move + 200, y_menu + 150):
         pokemon_trainer, pokemon_opponent, still_in_battle = battle_attack.perform_choice_attack(pokemon_trainer, pokemon_opponent, "move4", window,res_scene,resolution)
             
     return pokemon_trainer, pokemon_opponent, still_in_battle
