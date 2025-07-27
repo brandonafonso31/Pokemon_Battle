344fe0da0a895c9d6f0e874dadded5d1ea5efd72
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-27 17:37:01 +0200

    init some items

---
+++ item_init.py
@@ -0,0 +1,20 @@
+import item
+
+pokeball = item.Balls("Pokeball")
+superball = item.Balls("Super Ball")
+hyperball = item.Balls("Hyper Ball")
+masterball = item.Balls("Master Ball")
+rapidball = item.Balls("Rapide Ball")
+
+potion = item.Heals("Potion")
+super_potion = item.Heals("Super Potion")
+hyper_potion = item.Heals("Hyper Potion")
+max_potion = item.Heals("Max Potion")
+full_heal = item.Heals("Full Heal")
+
+antipara = item.Heal_Status("Antipara")
+reveil = item.Heal_Status("Réveil")
+antidote = item.Heal_Status("Antidote")
+rappel = item.Heal_Status("Rappel")
+maxrappel = item.Heal_Status("Max Rappel")
+
