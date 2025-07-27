20b30e6439a2e2f680b6011ae193e2426c97f093
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-27 13:08:25 +0200

    add txt support for button in ui_battle

--- ui_battle.py
+++ ui_battle.py
@@ -1,8 +1,8 @@
 from button import *
-
+from PIL import ImageColor
 def draw_move(window,move,x,y):
     """return a bool which is did the button got draw ?"""
     move_img_path = f"battle_ui/{move.type.name}_attack_button.png"
     move_img = pygame.image.load(move_img_path).convert_alpha()
-    move_button = Button(x, y, move_img, 1)
+    move_button = Button(x, y, move_img, 1,move.name, ImageColor.getrgb(move.type.color()))
     return move_button.draw(window)
