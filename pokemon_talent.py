b7fc2f4712c2582d1edd370e67c69efd647a7ab2
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-28 15:48:05 +0200

    add timing to talent to know when to activate it

--- pokemon_talent.py
+++ pokemon_talent.py
@@ -1,8 +1,11 @@
+from battle_timing import Timing
+
 class Talent:
-    def __init__(self, name, description, effect):
+    def __init__(self, name, description, effect, timing:Timing):
         self.name = name
         self.description = description
         self.effect = effect    # fonction callable that applies the effect
+        self.timing = timing
 
     def __str__(self):
         return f"{self.name}"
@@ -11,10 +14,10 @@ def torche():
     """Talent: Torche"""
     def effect(pokemon):
         # When hit by a Fire-type move, the Pokémon's Special Attack is raised by 1 stage.
-        pokemon.atk_spe += 1
+        pokemon.atk_spe += 1    #to change cause meh wrong implementation
         print(f"{pokemon.name}'s Special Attack rose!")
 
-    return Talent("Torche", "Boosts the Special Attack when hit by a Fire-type move.", effect)
+    return Talent("Torche", "Boosts the Special Attack when hit by a Fire-type move.", effect, Timing.ABOUT_TO_GET_HIT)
 
 talents = {
     "torche": torche,
