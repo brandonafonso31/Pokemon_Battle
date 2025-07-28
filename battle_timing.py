9d6f2880149919c8152f17fb0bee060504fe18fb
Brandon3118 <brandon.afonso3118@gmail.com>
2025-07-28 15:34:23 +0200

    add new timing : when about to get hit

--- battle_timing.py
+++ battle_timing.py
@@ -1,11 +1,13 @@
 from enum import Enum
 
 class Timing(Enum):
-    Start, GOT_HIT, END = range(3)
+    Start, ABOUT_TO_GET_HIT, GOT_HIT, END = range(4)
     
     def __str__(self):
         if self == Timing.Start:
             return "Début du tour"
+        elif self == Timing.ABOUT_TO_GET_HIT:
+            return "Attaque en cours"
         elif self == Timing.GOT_HIT:
             return "A subi des dégats"
         elif self == Timing.END:
