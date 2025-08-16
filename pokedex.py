b0a0b53c43dbf8e25b054bcec84bbba70bcc02f9
Brandon3118 <brandon.afonso3118@gmail.com>
2025-08-16 13:30:35 +0200

    add function to get pokemon infos from json

--- pokedex.py
+++ pokedex.py
@@ -6,6 +6,16 @@ from pokemon_talent import talents
 from config import pokemon_data_json_path
 import json
 
-def pokedex():
-    with open("pokemon_data_json_path","r") as f:
+def get_pokedex():
+    with open(pokemon_data_json_path,"r",encoding="utf-8") as f:
         return json.load(f)
+    
+def get_pokemon(id):
+    """Retourne le Pokémon dont le champ 'num' correspond à l'id"""
+    pokedex = get_pokedex()
+    for name, infos in pokedex.items():
+        if infos["num"] == id:
+            return infos
+    return None
+
+print(get_pokemon(6))
