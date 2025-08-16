6ac1c41270104b06cf6ae3e1da3a017cd542f0f6
Brandon3118 <brandon.afonso3118@gmail.com>
2025-08-16 13:22:25 +0200

    first function for pokedex and new var for pokedex file

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ img_dir_path = os.path.join(principal_dir_path,"images")
 song_dir_path = os.path.join(principal_dir_path,"song")
 data_dir_path = os.path.join(principal_dir_path,"data")
 battle_json_path = os.path.join(data_dir_path, "actual_battle.json")
+pokemon_data_json_path = os.path.join(data_dir_path, "pokedex.json")
 
 
 BLACK = (0, 0, 0, 255)
--- pokedex.py
+++ pokedex.py
@@ -3,7 +3,9 @@ from pokemon_type import Type
 from pokemon_move import *
 from pokemon_nature import Nature
 from pokemon_talent import talents
+from config import pokemon_data_json_path
+import json
 
 def pokedex():
-    with open("","") as f:
+    with open("pokemon_data_json_path","r") as f:
         return json.load(f)
