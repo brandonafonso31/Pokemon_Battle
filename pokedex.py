aaf12af512f026064b0c08b47a4c2400dfc75ca0
Brandon3118 <brandon.afonso3118@gmail.com>
2025-08-19 15:12:32 +0200

    now create_pokemon work

--- pokedex.py
+++ pokedex.py
@@ -8,9 +8,9 @@ import json,os
 
 
 def get_pokedex(num_gen: int = -1):
-    if num_gen != -1: path = f"data/pokedex_gen{num_gen}.json"
-    else: pokemon_data_json_path
-    with open(pokemon_data_json_path,"r",encoding="utf-8") as f:
+    if num_gen != -1: path = os.path.join(data_dir_path,f"pokedex_gen{num_gen}.json")
+    else: path = pokemon_data_json_path
+    with open(path,"r",encoding="utf-8") as f:
         return json.load(f)
 
 def get_pokemon(id,num_gen: int = -1):
@@ -19,9 +19,9 @@ def get_pokemon(id,num_gen: int = -1):
         if infos["num"] == id :  return infos
     return None
 
-def get_gen(id: str):
-    id = int(id)
-    if id <= 151: return 1
+def get_gen(id: int):
+    if id <= 0 : return None
+    elif id <= 151: return 1
     elif id <= 251: return 2
     elif id <= 386: return 3
     elif id <= 493: return 4
@@ -33,20 +33,24 @@ def get_gen(id: str):
     return None
 
 def create_pokemon(id,nature=Nature.BIZARRE,EV={"hp":0,"atk":0,"def_":0,"atk_spe":0,"def_spe":0,"vit":0},nickname="",num_on_sprite_sheet=1):
-    infos = get_pokemon(id)
+    num_gen = get_gen(id)
+    infos = get_pokemon(id,num_gen)
     if infos is None:
         return None
     
+    stats = infos["baseStats"]
+    types = infos["types"]
+        
     pokemon = Pokemon(name=infos["name"],
-                   hp=infos["baseStats"]["hp"],
-                   atk=infos["baseStats"]["atk"],
-                   def_=infos["baseStats"]["def"],
-                   atk_spe=infos["baseStats"]["spa"],
-                   def_spe=infos["baseStats"]["spd"],
-                   vit=infos["baseStats"]["spe"],
-                   gen=infos["gen"],
-                   type1=Type(infos["types"][0]),
-                   type2=Type(infos["types"][1]) if len(infos["types"]) > 1 else None,
+                   hp=stats["hp"],
+                   atk=stats["atk"],
+                   def_=stats["def"],
+                   atk_spe=stats["spa"],
+                   def_spe=stats["spd"],
+                   vit=stats["spe"],
+                   gen=num_gen,
+                   type1=Type[types[0]],
+                   type2=Type[types[1]] if len(types) > 1 else None,
                    num_on_sprite_sheet=num_on_sprite_sheet,
                    EV=EV,
                    nature=nature,                   
