7521f1f75e0fc138fc57f2cc136667c6dc3a61d9
Brandon3118 <brandon.afonso3118@gmail.com>
2025-08-21 21:19:01 +0200

    corr: some typo there and there

--- battle_timing.py
+++ battle_timing.py
@@ -2,10 +2,10 @@ from enum import Enum
 import threading
 
 class Timing(Enum):
-    Start, ABOUT_TO_GET_HIT, GOT_HIT, END = range(4)
+    START, ABOUT_TO_GET_HIT, GOT_HIT, END = range(4)
     
     def __str__(self):
-        if self == Timing.Start:
+        if self == Timing.START:
             return "Début du tour"
         elif self == Timing.ABOUT_TO_GET_HIT:
             return "Attaque en cours"
@@ -16,17 +16,16 @@ class Timing(Enum):
         return "Unknown Timing"
 
 def apply_timing_effect(pokemon_using_talent, pokemon_2):
-    timing_talent = pokemon_using_talent.talent.timing
     talent = pokemon_using_talent.talent
+    timing_talent = talent.timing
     with timing_lock:
-        #print(f"Timing actuel: {current_timing}, Timing talent: {timing_talent}")
         if current_timing == timing_talent:
             talent.trigger(pokemon_using_talent, pokemon_2)
             
-    return pokemon_using_talent, pokemon_2 
+    return pokemon_using_talent, pokemon_2
     
 def check_timing_talent(pokemon_1, pokemon_2):
-    """Check if the timing is correct for the move"""
+    """Check if the timing is correct for the talent"""
     if pokemon_1.vit >= pokemon_2.vit:
         pokemon_1, pokemon_2 = apply_timing_effect(pokemon_1, pokemon_2)
         pokemon_2, pokemon_1 = apply_timing_effect(pokemon_2, pokemon_1)
