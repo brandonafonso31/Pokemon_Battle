bc4e8050d39a8f6f2158cc5f523e7229c85c1ca0
brandonafonso31 <brandon.afonso3118@gmail.com>
2025-08-24 17:53:15 +0200

    changing font color by default to white

--- button_test.py
+++ button_test.py
@@ -2,7 +2,7 @@ import pygame
 from config import WHITE
 
 class Button_test:
-    def __init__(self, x, y, image, scale, text='', text_color=(0, 0, 0), padding=10):
+    def __init__(self, x, y, image, scale, text='', text_color=WHITE, padding=10):
         width = image.get_width()
         height = image.get_height()
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
@@ -13,6 +13,7 @@ class Button_test:
         self.text = text
         self.text_color = text_color
         self.font_name = pygame.font.get_default_font()
+        self.font = pygame.font.SysFont(self.font_name, 50) 
         self.padding = padding
         self.text_surface, self.text_rect = self.render_text_to_fit()
   
@@ -21,17 +22,20 @@ class Button_test:
         max_height = self.rect.height - 2 * self.padding
         adjust_x = 2.5
         font_size = 50  # Taille max initiale
+
         while font_size > 20:
             font = pygame.font.SysFont(self.font_name, font_size)
             text_surface = font.render(self.text, True, self.text_color)
             text_rect = text_surface.get_rect()
             if text_rect.width <= max_width and text_rect.height <= max_height:
-                text_rect.center = (self.rect.center[0] - adjust_x, self.rect.center[1])  # adjust bc image not symmetrical
+                text_rect.center = (self.rect.center[0] - adjust_x, self.rect.center[1])
+                self.font = font
                 return text_surface, text_rect
             font_size -= 1
 
         # Fallback : texte trop long
         font = pygame.font.SysFont(self.font_name, 10)
+        self.font = font
         text_surface = font.render(self.text, True, self.text_color)
         text_rect = text_surface.get_rect(center=(self.rect.center[0] - adjust_x, self.rect.center[1]))
         return text_surface, text_rect
@@ -39,7 +43,7 @@ class Button_test:
     def draw(self, window):
         window.blit(self.image, (self.rect.x, self.rect.y))
         window.blit(self.text_surface, self.text_rect)
-    
+        
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             return event.button == 1 and self.rect.collidepoint(event.pos)
